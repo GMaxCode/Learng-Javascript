f12c06f61782c6ca33035f4d99dccd0ee7ed8667
Guilherme Maximo <guilhermegomesmaximo@gmail.com>
2026-09-11 00:10:20 -0300

    exercicio de variaveis com const e let

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+const nome = 'Guilherme'; // const é para variaveis que não mudam! receberam sempre aquele dado e ponto
+
+const idade = 20;
+
+let saldo = 100; // let é para variaveis mutaveis, que podem ter seu dado mudado mais pra frente.
+
+saldo = saldo + 50 
+
+console.log(`seu nome é ${nome} e você tem ${idade}`) // Crase ao contrário para fazer a Concatenação, pegar alguma variavel e por dentro do console.log
+console.log(`você tem ${saldo}`) 
