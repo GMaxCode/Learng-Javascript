c9c41ed42dd49d6f0af142fbac043bf44ae4b8cb
Guilherme Maximo <guilhermegomesmaximo@gmail.com>
2026-09-15 01:13:08 -0300

    aprendendo a usar ELSE IF

---
+++ aula3.js
@@ -0,0 +1,18 @@
+const boca = 1;
+const perna = 9;
+const abelha = 18;
+
+const soma = abelha + perna;
+soma_si = boca + perna;
+
+console.log(`a soma de ${boca} + ${perna} = ${soma_si}`);
+console.log(soma);
+
+if (soma_si <= 10 ) {
+    console.log("Boa Prática");
+} else if (soma_si <= 15 ) {
+    console.log("Boa Noite");
+} else {
+    console.log("Não sobrou nada pros Betinho");
+}
+
